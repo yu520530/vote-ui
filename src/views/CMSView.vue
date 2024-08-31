11836ce357f1c9fa1572f019ab559818e73bce40
yo <ya90312@gmail.com>
2024-09-01 01:23:32 +0800

    feat: 更改AboutView>CMSView 並且新增table

---
+++ src/views/CMSView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+      <h1 class="text-center">投票項目</h1>
+      <table class="table">
+        <thead>
+        <th>投票項目編號</th><th>投票項目名稱</th>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </div>
+</template>
+<script setup lang="ts">
+</script>
