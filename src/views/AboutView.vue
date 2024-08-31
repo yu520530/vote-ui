40c730cc322af14df7c4c3f4964ea36d60dd2997
yo <ya90312@gmail.com>
2024-09-01 01:14:44 +0800

    feat: 安裝router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>投票項目</h1>
+    <table class="text-center">
+      <thead>
+        <th>投票項目編號</th><th>投票項目名稱</th>
+      </thead>
+
+    </table>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    Home page
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
